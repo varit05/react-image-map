1900eb94e432397fc3fd7cd2e924edb36e4c4130
varit05 <varitpatel@gmail.com>
2020-04-05 15:16:48 +0530

    react pan zoom changes

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,7 +1,21 @@
 import React from "react";
 import { data } from "../images-data";
 import { Controls } from "./Controls";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+import { ReactPanZoom } from "./react-pan-zoom-rotate";
+
+const Container = css`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+  & img {
+    width: 100%;
+  }
+`;
 
 const Img = styled.img`
   width: ${props =>
@@ -17,8 +31,14 @@ const Img = styled.img`
   max-height: 256px;
   margin-bottom: -3px;
 `;
-
+const StyledReactPanZoom = styled(ReactPanZoom)`
+  ${Container};
+`;
 export const MapView = ({ zoom }) => {
+  const [dx, setDx] = React.useState(0);
+  const [dy, setDy] = React.useState(0);
+  const [rotation] = React.useState(0);
+
   const imagesToLoad = data.filter(imagePath => {
     return imagePath.zoom === zoom;
   });
@@ -26,25 +46,40 @@ export const MapView = ({ zoom }) => {
   console.log("MapView -> imagesToLoad", imagesToLoad);
   const imageDetails = imagesToLoad.length ? imagesToLoad[0].image : "";
 
+  const onPan = (dx, dy) => {
+    setDx(dx);
+    setDy(dy);
+  };
+
   return (
     <>
-      <Controls></Controls>
+      <Controls zoom={zoom}></Controls>
       <div className="image-map">
-        {imageDetails.map(image => (
-          <div>
-            {" "}
-            {image.length
-              ? image.map((img, index) => (
-                  <Img
-                    zoom={zoom}
-                    src={require(`${img}`)}
-                    alt="product"
-                    key={index}
-                  />
-                ))
-              : ""}{" "}
-          </div>
-        ))}
+        <StyledReactPanZoom
+          zoom={zoom}
+          pandx={dx}
+          pandy={dy}
+          onPan={onPan}
+          rotation={rotation}
+          key={dx}
+          enablePan={true}
+        >
+          {imageDetails.map(image => (
+            <div>
+              {" "}
+              {image.length
+                ? image.map((img, index) => (
+                    <Img
+                      zoom={zoom}
+                      src={require(`${img}`)}
+                      alt="product"
+                      key={index}
+                    />
+                  ))
+                : ""}{" "}
+            </div>
+          ))}
+        </StyledReactPanZoom>
       </div>
     </>
   );
--- src/components/react-pan-zoom-rotate.js
+++ src/components/react-pan-zoom-rotate.js
@@ -1,80 +1,52 @@
-import * as React from "react";
+import React, { useState } from "react";
 
-const ReactPanZoom = () => {
-  // In strict null checking setting default props doesn't seem to work. Hence the non-null assertion.
-  // :crossedfingers: it shouldn't be deprecated. Or the very least support defaultProps semantics as proposed
-  // in this PR: https://github.com/Microsoft/TypeScript/issues/23812
-  // let defaultProps = {
-  //   enablePan: true,
-  //   onPan: () => undefined,
-  //   onReset: () => undefined,
-  //   pandx: 0,
-  //   pandy: 0,
-  //   zoom: 0,
-  //   rotation: 0,
+export const ReactPanZoom = props => {
+  let panWrapper;
+  let panContainer;
+  const { pandx, pandy, zoom } = props;
+  const defaultDragData = {
+    dx: pandx || 0,
+    dy: pandy || 0,
+    x: 0,
+    y: 0
+  };
+
+  const defaultMatrixData = [zoom, 0, 0, zoom || 2.2, pandx || 0, pandy || 0];
+
+  console.log("rect-pan-zoom-rotate", props);
+
+  const [comesFromDragging, setFromDragging] = useState(false);
+  const [dragging, setDragging] = useState(false);
+  const [mouseDown, setMouseDown] = useState(false);
+  const [dragData, setDragData] = useState(defaultDragData);
+  const [matrixData, setMatrixData] = useState(defaultMatrixData);
+
+  // componentWillReceiveProps = nextProps => {
+  //   const { matrixData } = state;
+  //   if (matrixData[0] !== nextProps.zoom) {
+  //     const newMatrixData = [...matrixData];
+  //     newMatrixData[0] = nextProps.zoom || newMatrixData[0];
+  //     newMatrixData[3] = nextProps.zoom || newMatrixData[3];
+  //     this.setState({
+  //       matrixData: newMatrixData
+  //     });
+  //   }
   // };
-  // let panWrapper;
-  // let panContainer;
-
-  const getInitialState = props => {
-    const { pandx, pandy, zoom } = this.props;
-    const defaultDragData = {
-      dx: pandx,
-      dy: pandy,
-      x: 0,
-      y: 0
-    };
-    return {
-      comesFromDragging: false,
-      dragData: defaultDragData,
-      dragging: false,
-      matrixData: [
-        zoom,
-        0,
-        0,
-        zoom,
-        pandx,
-        pandy // [zoom, skew, skew, zoom, dx, dy]
-      ],
-      mouseDown: false
-    };
-  };
-
-  const state = getInitialState();
-
-  componentWillReceiveProps = nextProps => {
-    const { matrixData } = state;
-    if (matrixData[0] !== nextProps.zoom) {
-      const newMatrixData = [...state.matrixData];
-      newMatrixData[0] = nextProps.zoom || newMatrixData[0];
-      newMatrixData[3] = nextProps.zoom || newMatrixData[3];
-      this.setState({
-        matrixData: newMatrixData
-      });
-    }
-  };
-
-  const reset = () => {
-    const matrixData = [0.4, 0, 0, 0.4, 0, 0];
-    this.setState({ matrixData });
-    if (this.props.onReset) {
-      this.props.onReset(0, 0, 1);
-    }
-  };
 
   const onClick = e => {
-    if (state.comesFromDragging) {
+    if (comesFromDragging) {
       return;
     }
 
-    if (this.props.onClick) {
-      this.props.onClick(e);
+    if (props.onClick) {
+      props.onClick(e);
     }
   };
 
   const onTouchStart = e => {
+    console.log("touches for onTouchStart ", e.touches[0]);
     const { pageX, pageY } = e.touches[0];
-    this.panStart(pageX, pageY, e);
+    panStart(pageX, pageY, e);
   };
 
   const onTouchEnd = e => {
@@ -86,14 +58,13 @@ const ReactPanZoom = () => {
   };
 
   const onMouseDown = e => {
-    this.panStart(e.pageX, e.pageY, e);
+    panStart(e.pageX, e.pageY, e);
   };
   const panStart = (pageX, pageY, event) => {
-    if (!this.props.enablePan) {
+    if (!props.enablePan) {
       return;
     }
 
-    const { matrixData } = state;
     const offsetX = matrixData[4];
     const offsetY = matrixData[5];
     const newDragData = {
@@ -102,12 +73,10 @@ const ReactPanZoom = () => {
       x: pageX,
       y: pageY
     };
-    this.setState({
-      dragData: newDragData,
-      mouseDown: true
-    });
-    if (this.panWrapper) {
-      this.panWrapper.style.cursor = "move";
+    setDragData(newDragData);
+    setMouseDown(true);
+    if (panWrapper) {
+      panWrapper.style.cursor = "move";
     }
     event.stopPropagation();
     event.nativeEvent.stopImmediatePropagation();
@@ -115,40 +84,37 @@ const ReactPanZoom = () => {
   };
 
   const onMouseUp = e => {
-    this.panEnd(e);
+    panEnd(e);
   };
   const panEnd = e => {
-    this.setState({
-      comesFromDragging: state.dragging,
-      dragging: false,
-      mouseDown: false
-    });
-    if (this.panWrapper) {
-      this.panWrapper.style.cursor = "";
+    setFromDragging(dragging);
+    setDragging(false);
+    setMouseDown(false);
+    if (panWrapper) {
+      panWrapper.style.cursor = "";
     }
-    if (this.props.onPan) {
-      this.props.onPan(state.matrixData[4], state.matrixData[5]);
+    if (props.onPan) {
+      props.onPan(matrixData[4], matrixData[5]);
     }
   };
 
   const onMouseMove = e => {
     updateMousePosition(e.pageX, e.pageY);
   };
+
   const updateMousePosition = (pageX, pageY) => {
-    if (!state.mouseDown) return;
+    if (!mouseDown) return;
 
     const matrixData = getNewMatrixData(pageX, pageY);
-    this.setState({
-      dragging: true,
-      matrixData
-    });
-    if (this.panContainer) {
-      this.panContainer.style.transform = `matrix(${this.state.matrixData.toString()})`;
+    setDragging(true);
+    setMatrixData(matrixData);
+
+    if (panContainer) {
+      this.panCont.style.transform = `matrix(${matrixData.toString()})`;
     }
   };
 
   const getNewMatrixData = (x, y) => {
-    const { dragData, matrixData } = this.state;
     const deltaX = dragData.x - x;
     const deltaY = dragData.y - y;
     matrixData[4] = dragData.dx - deltaX;
@@ -158,7 +124,7 @@ const ReactPanZoom = () => {
 
   return (
     <div
-      className={`pan-container ${this.props.className || ""}`}
+      className={`pan-container ${props.className || ""}`}
       onMouseDown={onMouseDown}
       onMouseUp={onMouseUp}
       onTouchStart={onTouchStart}
@@ -167,19 +133,19 @@ const ReactPanZoom = () => {
       onMouseMove={onMouseMove}
       onClick={onClick}
       style={{
-        height: this.props.height,
+        height: props.height,
         userSelect: "none",
-        width: this.props.width
+        width: props.width
       }}
-      ref={ref => (this.panWrapper = ref)}
+      ref={ref => (panWrapper = ref)}
     >
       <div
-        ref={ref => (ref ? (this.panContainer = ref) : null)}
+        ref={ref => (ref ? (panContainer = ref) : null)}
         style={{
-          transform: `matrix(${state.matrixData.toString()})`
+          transform: `matrix(${matrixData.toString()})`
         }}
       >
-        {this.props.children}
+        {props.children}
       </div>
     </div>
   );
